666b5615042c898676bceaf2931851e5a878a389
Aaron Brooks <aaron@brooks1.net>
2013-11-05 15:19:29 -0500

    Start implementing arguments - :print :long-sha

--- src/leiningen/git_version.clj
+++ src/leiningen/git_version.clj
@@ -13,8 +13,10 @@
            t))
 
 (defn get-git-version
-  [path]
-  (let [{:keys [out exit err]} (sh "git" "log" "-1" "--pretty=%h,%cd" path)
+  [path & [long-sha]]
+  (let [shafmt (if long-sha "%H" "%h")
+        fmt (str "--pretty=" shafmt ",%cd")
+        {:keys [out exit err]} (sh "git" "log" "-1" fmt path)
         ;; Throw exception if error?
         [sha, datestr] (-> out s/trim (s/split #"," 2))
         ctime (Date. datestr)]
@@ -29,13 +31,16 @@
   [project & args]
   (let [[kstrs sargs] (split-with #(.startsWith % ":") args)
         kargs (map #(keyword (.substring % 1)) kstrs)
-        gver (-> project :root get-git-version)
+        long-sha (some #{:long-sha} kargs)
+        gver (-> project :root (get-git-version long-sha))
         qual (format-git-ver gver timestamp-fmt)
-        upfn #(s/replace % #"-SNAPSHOT" qual)
+        upfn #(str (s/replace % #"-SNAPSHOT" "") qual)
         nproj (update-in project [:version] upfn)
         nmeta (update-in (meta project) [:without-profiles :version] upfn)
         nnproj (with-meta nproj nmeta)]
     ;; TODO throw exception if dirty
     ;; TODO throw exception if upstream doesn't contain this commit
-    ;; args :insanely-allow-dirty-working-copy :no-upstream
-    (lmain/resolve-and-apply nnproj sargs)))
+    ;; args :insanely-allow-dirty-working-copy :no-upstream :print :long-sha
+    (if (some #{:print} kargs)
+      (println (upfn (:version project)))
+      (lmain/resolve-and-apply nnproj sargs))))
