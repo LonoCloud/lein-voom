38c20d9a01942d1a665e7193685a18627650d745
Aaron Brooks <aaron@brooks1.net>
2013-11-04 22:38:48 -0500

    Basic working version.
    This provides none of the intended arguments and doesn't handle git
    shell-out errors.
    
    It does however shift any -SNAPSHOT version strings to -[CTIME]-[SHA]
    where CTIME is the commit time of the most recent change of this
    project's subdirectory and SHA is the sha of that commit.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject lein-git-version "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[clj-jgit "0.6.1"]]
+  :eval-in-leiningen true)
---
+++ src/leiningen/git_version.clj
@@ -0,0 +1,45 @@
+(ns leiningen.git-version
+  [:use [clojure.pprint]]
+  (:require [clj-jgit.querying :as gitq]
+            [clj-jgit.porcelain :as git]
+            [clojure.java.shell :refer [sh]]
+            [clojure.string :as s]
+            [leiningen.core.main :as lmain])
+  (:import [java.util Date]))
+
+(def timestamp-fmt "yyyyMMddhhmmss")
+
+(defn formatted-timestamp
+  [fmt t]
+  (.format (doto (java.text.SimpleDateFormat. fmt java.util.Locale/US)
+             (.setTimeZone (java.util.SimpleTimeZone. 0 "GMT")))
+           t))
+
+(defn get-git-version
+  [path]
+  (let [{:keys [out exit err]} (sh "git" "log" "-1" "--pretty=%h,%cd" path)
+        ;; Throw exception if error?
+        [sha, datestr] (-> out s/trim (s/split #"," 2))
+        ctime (Date. datestr)]
+    {:ctime ctime :sha sha}))
+
+(defn format-git-ver
+  [gver fmt]
+  (let [{:keys [ctime sha]} gver]
+    (str "-" (formatted-timestamp fmt ctime) "-" sha)))
+
+(defn git-version
+  [project & args]
+  (let [[kstrs sargs] (split-with #(.startsWith % ":") args)
+        kargs (map #(keyword (.substring % 1)) kstrs)
+        gcommit (git/load-repo ".")
+        gver (-> project :root get-git-version)
+        qual (format-git-ver gver timestamp-fmt)
+        upfn #(s/replace % #"-SNAPSHOT" qual)
+        nproj (update-in project [:version] upfn)
+        nmeta (update-in (meta project) [:without-profiles :version] upfn)
+        nnproj (with-meta nproj nmeta)]
+    ;; TODO throw exception if dirty
+    ;; TODO throw exception if upstream doesn't contain this commit
+    ;; args :insanely-allow-dirty-working-copy :no-upstream
+    (lmain/resolve-and-apply nnproj sargs)))
