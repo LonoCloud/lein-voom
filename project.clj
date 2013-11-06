fa8c8e4632ddf389e2d830a25af1ac9ccd65667b
Aaron Brooks <aaron@brooks1.net>
2013-11-06 10:50:01 -0500

    Remove jgit dependency from project file (missed in earlier purge).

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-jgit "0.6.1"]]
   :eval-in-leiningen true)
