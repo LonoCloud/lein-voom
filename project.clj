c1a843a6dccd838fd9345fbfcf09822efc911cf8
Aaron Brooks <aaron@brooks1.net>
2013-11-07 15:26:08 -0500

    Largely working merge of dep resolving and dep building.
    This commit will now walk a projects dependencies, looking for
    versioned artifacts, resolving them or building them (if possible) if
    they are unresolvable.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject resolve-git-version "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[lein-git-version "0.1.0-SNAPSHOT"]]
+  :dependencies [[clj-glob "1.0.0"]
+                 [lein-git-version "0.1.0-SNAPSHOT"]]
   :eval-in-leiningen true)
--- src/leiningen/resolve_git_version.clj
+++ src/leiningen/resolve_git_version.clj
@@ -1,11 +1,94 @@
 (ns leiningen.resolve-git-version
   (:require [cemerick.pomegranate.aether :as aether]
+            [clojure.java.shell :refer [sh]]
             [clojure.pprint :refer [pprint]]
-            [leiningen.git-version :as gv])
+            [clojure.string :as s]
+            [leiningen.core.project :as project]
+            [leiningen.git-version :as gv]
+            [org.satta.glob :refer [glob]])
   (:import (org.sonatype.aether.transfer ArtifactNotFoundException)
            (java.util.logging Logger Handler Level)))
 
 (set! *warn-on-reflection* true)
+
+(def name-in-transition "ova")
+
+(def repos-home (or (System/getenv "REPOS_HOME")
+                    (str (System/getProperty "user.home")
+                         (str "/." name-in-transition "-repos"))))
+
+(defn with-log-level [level f]
+  (let [handlers (doall (.getHandlers (Logger/getLogger "")))
+        old-levels (doall (map #(.getLevel ^Handler %) handlers))
+        _ (doseq [h handlers] (.setLevel ^Handler h level))
+        result (f)]
+    (dorun (map #(.setLevel ^Handler %1 %2) handlers old-levels))
+    result))
+
+(defn git
+  [d & subcmd]
+  ;; We won't handle bare repos or displaced worktrees
+  (let [gitdir (.getPath d)
+        worktree (.getParent d)]
+    (apply sh "git" (str "--git-dir=" gitdir) (str "--work-tree=" worktree) subcmd)))
+
+(defn find-project-files
+  [d]
+  (let [{:keys [out err exit]} (git d "ls-files" "project.clj" "**/project.clj")
+        projects (when-not (empty? out)
+                   (s/split-lines out))]
+    (map #(str (.getParent d) "/" %) projects)))
+
+(defn contains-sha? [d sha]
+  (prn "contains-sha?" d sha)
+  (->>
+   (git d "rev-parse" "--verify" "--quiet" sha)
+   :exit
+   zero?))
+
+(defn locate-sha
+  [dirs sha]
+  (seq (for [d dirs
+             :when (contains-sha? d sha)]
+         d)))
+
+(defn fetch-all
+  [dirs]
+  (doseq [d dirs]
+    (println "Fetching:" (.getPath d))
+    (git d "fetch")))
+
+(defn find-project
+  [pgroup pname candidate]
+  (prn "find-project" pgroup pname candidate)
+  (for [p (find-project-files candidate)
+        :let [{:keys [group name] :as prj} (project/read p)]
+        :when (and (= group pgroup)
+                   (= name pname))]
+    prj))
+
+(defn find-matching-projects
+  "[project {groupId name ctime sha}] [project coordinate-string]"
+  [repos-dir pspec]
+  (prn "find-matching-projects" repos-dir pspec)
+  (let [{:keys [sha artifactId groupId]} pspec
+        dirglob (str (s/replace repos-dir #"/$" "") "/*/.git")
+        dirs (glob dirglob)
+        _ (prn "dirs" dirglob dirs)
+        sha-candidates (locate-sha dirs sha)
+        sha-candidates (or sha-candidates
+                           (do (fetch-all dirs)
+                               (locate-sha dirs sha)))]
+    (prn "sha-candidates" sha-candidates)
+    (mapcat (fn [c]
+              (git c "checkout" sha)
+              (find-project groupId artifactId c))
+            sha-candidates)))
+
+(defn install-versioned-artifact
+  [proot]
+  (let [r (sh "lein" "git-version" "install" :dir proot)]
+    (prn "r" r)))
 
 (defn missing-artifacts-from-exception
   "Returns a sequence of artifacts indicated as missing anywhere in
@@ -21,29 +104,31 @@
   "Build and install given artifact using git. Return value is
   undefined. Throws an exception with detailed message if artifact
   cannot be resolved."
-  [old-exception {:keys [version] :as art}]
+  [old-exception {:keys [artifactId version] :as art}]
 
-  (if-let [{:keys [ctime sha]} (gv/ver-parse version)]
-    (do
-      (println "TODO: Build and install git version for artifact" (pr-str art))
-      (throw (ex-info "TODO" {:ctime ctime :sha sha} old-exception)))
+  (if-let [vmap (gv/ver-parse version)]
+    (let [prjs (find-matching-projects repos-home (merge vmap art))]
+      (when (empty? prjs)
+        (throw (ex-info (str "No project found for " artifactId " " version
+                             " (Hint: might need to clone a new repo into "
+                             repos-home ")")
+                        {:artifact art :vmap vmap} old-exception)))
+      (when (< 1 (count prjs))
+        (println "WARNING: multiple projects match" artifactId ":" )
+        (doseq [prj prjs]
+          (println "->" (:root prj))))
+      (doseq [prj prjs]
+        (install-versioned-artifact (:root prj))))
     (throw (ex-info (str "Not parseable as git-version: " version) {:artifact art} old-exception))))
 
-(defn with-log-level [level f]
-  (let [handlers (doall (.getHandlers (Logger/getLogger "")))
-        old-levels (doall (map #(.getLevel ^Handler %) handlers))
-        _ (doseq [h handlers] (.setLevel ^Handler h level))
-        result (f)]
-    (dorun (map #(.setLevel ^Handler %1 %2) handlers old-levels))
-    result))
-
 (def null-writer
+  "Like /dev/null, but for Java!"
   (proxy [java.io.Writer] []
     (write ([_]) ([_ _ _]))
     (flush [])
     (close [])))
 
-(defn try-once-resolve-git-version [project]
+(defn try-once-resolve-ova-version [project]
   (try
     (with-log-level Level/OFF
       #(binding [*err* null-writer]
@@ -58,13 +143,12 @@
           (resolve-artifact e art))
         (throw e)))))
 
-(defn resolve-git-version
+(defn resolve-ova-version
   "Resolves project dependencies like 'lein deps', but also uses TOOL_REPOS_DIR"
   [project & args]
-
   (try
     (loop []
-      (when-not (= :ok (try-once-resolve-git-version project))
+      (when-not (= :ok (try-once-resolve-ova-version project))
         (recur)))
     (catch clojure.lang.ExceptionInfo e
       (println "Failed to resolve dependency:" (.getMessage e))
