1caaf78fcf02b25e40223543a09df63d7bcec08e
Chouser <chouser@lonocloud.com>
2013-12-04 15:32:43 -0500

    Update project.clj decription, url, signing key

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject lein-voom "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Tool for generating artifacts versioned on the most recent git commit sha and commit time."
+  :url "https://github.com/LonoCloud/lein-voom"
+  :scm {:name "git"
+        :url "https://github.com/LonoCloud/lein-voom"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-glob "1.0.0"]]
+  :signing {:gpg-key "A4D5C342"}
   :eval-in-leiningen true)
