9db18a8086de33a84d04f0db8e605108528da592
Aaron Brooks <abrooks@lonocloud.com>
2013-12-22 22:29:57 -0500

    Split shabam/bitmap routines out in to separate namespace.

--- src/leiningen/voom.clj
+++ src/leiningen/voom.clj
@@ -13,11 +13,13 @@
             [leiningen.core.main :as lmain]
             [leiningen.voom.long-sha :as sha :only [mk]]
             [leiningen.voom.pldb :as vdb]
+            [leiningen.voom.shabam :refer [shabam-new shabam-contains? shabam-add
+                                           sha-ancestors sha-successors]]
             [lonocloud.synthread :as ->]
             [org.satta.glob :refer [glob]]
             [robert.hooke :as hooke])
   (:import [clojure.lang #_IPersistentVector Seqable]
-           [java.util Date Arrays]
+           [java.util Date]
            [java.lang.reflect Array]
            [java.io File FileInputStream FileOutputStream OutputStreamWriter
             Closeable]
@@ -882,87 +884,6 @@
               :let [[_ ftype sha fname] (s/split (s/trim e) #"\s" 4)]]
           [fname {:sha sha
                   :ftype ftype}])))
-
-;; ===== ancestry sha-bitmap (shabam) =====
-
-(def ^Long bits-per-byte 8)
-(def ^Long bytes-per-long 8)
-(def ^Long bits-per-long (* bits-per-byte bytes-per-long))
-
-(defn ^Long bm-longs [bits]
-  (long (Math/ceil (/ bits bits-per-long))))
-
-(defn ^"[J" bm-new []
-  (long-array 1))
-
-(defn ^"[J" bm-set
-  [^"[J" bm idx]
-  (let [size (max (count bm) (bm-longs (inc idx)))
-        nbm (Arrays/copyOf bm ^Long size)
-        w (quot idx bits-per-long)
-        o (mod idx bits-per-long)
-        m (bit-set 0 o)
-        v (aget nbm w)
-        nv (bit-or v ^Long m)]
-    (aset nbm w nv)
-    nbm))
-
-(defn ^Long bm-get
-  [^"[J" bm idx]
-  (when (<= (bm-longs (inc idx)) (count bm))
-    (let [w (quot idx bits-per-long)
-          o (mod idx bits-per-long)
-          m (bit-set 0 o)
-          v (aget bm w)
-          mv (bit-and v m)]
-      (pos? mv))))
-
-(defn ^"[J" bm-or
-  [& bms]
-  (if (empty? bms)
-    (bm-new)
-    (let [size (apply max (map count bms))
-          nbm (Arrays/copyOf ^"[J" (first bms) ^Long size)]
-      (doseq [bm (rest bms)
-              [i v] (map-indexed list bm)
-              :let [mv (bit-or v (aget nbm i))]]
-        (aset nbm i mv))
-      nbm)))
-
-(defn shabam-new []
-  {:sha->idx {} :bitmaps []})
-
-(defn shabam-contains? [shabam sha]
-  (-> shabam :sha->idx (contains? sha)))
-
-(defn shabam-add [shabam sha & parents]
-  (if (shabam-contains? shabam sha)
-    shabam
-    (let [{:keys [sha->idx bitmaps]} shabam
-          nid (count sha->idx)
-          sha->idx (assoc sha->idx sha nid)
-          pidxs (map sha->idx parents)
-          nbm (if (empty? pidxs)
-                (bm-new)
-                (apply bm-or (map bitmaps pidxs)))
-          nbm (reduce bm-set nbm pidxs)
-          bitmaps (conj bitmaps nbm)]
-      {:sha->idx sha->idx
-       :bitmaps bitmaps})))
-
-(defn sha-ancestors [{:keys [sha->idx bitmaps]} child ancs]
-  (if-let [cidx (sha->idx child)]
-    (filter #(when-let [sha (sha->idx %)]
-               (bm-get (get bitmaps cidx)
-                       sha))
-            ancs)))
-
-(defn sha-successors [{:keys [sha->idx bitmaps]} parent succ]
-  (if-let [pidx (sha->idx parent)]
-    (filter #(when-let [sha (sha->idx %)]
-               (bm-get (get bitmaps sha)
-                       pidx))
-            succ)))
 
 ;; ===== relation database tables =====
 
