LonoCloud/lein-voom

commit d60e185c6b2ef511d19f8cd9ab179268c382f68c

Use relative links when creating project link in box-add.
The previous behavior used absolute symlinks. This change allows box
directories to be renamed and moved.

src/leiningen/voom.clj
@@ -651,6 +651,7 @@
           pname (proj-id->name proj)
           pdir (adj-path bdir pname)
           checkout (adj-path bdir task-dir pname)
+          rel-checkout (str task-dir "/" pname)
           g {:gitdir checkout}]
       (when (and (.exists ^File checkout)
                  (not= repo (-> (remotes (:gitdir g)) :origin :fetch)))
@@ -663,7 +664,7 @@
       (git g "checkout" branch)
       (sh "rm" "-f" pdir)
       ;; (SYMLINK WILL NEED TO BE UPDATED FOR EACH CHECKOUT)
-      (sh "ln" "-s" (adj-path checkout path) pdir))
+      (sh "ln" "-s" rel-checkout pdir))
     (println "Can't find box")))
 
 (defn fold-args-as-meta

commit 9e4f9f4ba6cc8eaea8c4cd8256db697bfe27fee7

Fix issue #12: box link points to top of repo.
This was an issue introduced in commit d60e185c, making the box
project symlinks relative.

src/leiningen/voom.clj
@@ -651,7 +651,7 @@
           pname (proj-id->name proj)
           pdir (adj-path bdir pname)
           checkout (adj-path bdir task-dir pname)
-          rel-checkout (str task-dir "/" pname)
+          rel-checkout (adj-path (File. ^String task-dir) pname path)
           g {:gitdir checkout}]
       (when (and (.exists ^File checkout)
                  (not= repo (-> (remotes (:gitdir g)) :origin :fetch)))