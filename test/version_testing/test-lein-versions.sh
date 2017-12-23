d732f2a6e15799a49f8b8677ddaba9dd351e7c0b
Aaron Brooks <aaron.brooks@viasat.com>
2017-12-23 14:01:03 -0500

    [version_testing] Handle all tests in scripts/ directory as default.

--- test/version_testing/test-lein-versions.sh
+++ test/version_testing/test-lein-versions.sh
@@ -6,7 +6,7 @@ LEIN_VOOM_VERSION=${LEIN_VOOM_VERSION:-0.1.0-20171223_184737-g8fc284c}
 # lein-voom currently generates a stacktrace for lein 2.8.1
 #LEIN_TEST_VERSIONS=${LEIN_TEST_VERSIONS:-2.6.1 2.7.1 2.8.1}
 LEIN_TEST_VERSIONS=${LEIN_TEST_VERSIONS:-2.6.1 2.7.1}
-LEIN_TESTS=${LEIN_TESTS:-scripts/lcp_test.sh}
+LEIN_TESTS=${LEIN_TESTS:-$(echo scripts/*)}
 
 for ver in $LEIN_TEST_VERSIONS; do
     docker build -t lein-test:${ver} --build-arg LEIN_VER=${ver} --build-arg VOOM_VER=${LEIN_VOOM_VERSION} .
