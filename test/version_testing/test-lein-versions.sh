19db14da3b1d57614ce04d913774fbe47995dc83
Aaron Brooks <aaron.brooks@viasat.com>
2017-12-25 18:22:01 -0500

    [version_testing] Create test images with pre-installed lein and lein-voom.

--- test/version_testing/test-lein-versions.sh
+++ test/version_testing/test-lein-versions.sh
@@ -7,6 +7,8 @@ LEIN_TEST_VERSIONS=${LEIN_TEST_VERSIONS:-2.6.1 2.7.1 2.8.1}
 LEIN_TESTS=${LEIN_TESTS:-$(echo scripts/*)}
 
 for ver in $LEIN_TEST_VERSIONS; do
+    # Attempt to copy in the appropriate pre-fetched or locally built lein-voom
+    cp -a ~/.m2/repository/lein-voom/lein-voom/. artifacts/lein-voom/lein-voom/. || true
     docker build -t lein-test:${ver} --build-arg LEIN_VER=${ver} --build-arg VOOM_VER=${LEIN_VOOM_VERSION} .
     for test in $LEIN_TESTS; do
         docker run -i --rm \
